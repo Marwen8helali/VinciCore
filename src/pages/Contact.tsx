d202b6274f285c79e46f711bd65dcf337c4b00c1
Marwen8helali <60137558+Marwen8helali@users.noreply.github.com>
2025-09-30 23:32:46 +0200

    Update Contact.tsx
    add url webserver

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -66,7 +66,7 @@ const Contact = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://vincicore-fullstack-project.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
